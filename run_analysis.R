45b4db98266bf4380da6baa74d43a9b51a61dc60
alexis <alexis@vgr-debian-4.(none)>
2014-04-27 21:39:40 +0300

    Working at run_analysis.R file

--- run_analysis.R
+++ run_analysis.R
@@ -1,102 +1,113 @@
-# This script does the following
-# 1.  Merges the training and the test sets to create one data set.
-# 2.	Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3.	Uses descriptive activity names to name the activities in the data set
-# 4.	Appropriately labels the data set with descriptive activity names. 
-# 5.	Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-# reshape2 is an R package written by Hadley Wickham that makes it easy 
-# to transform data between wide and long formats.
-library(reshape2)
+# 01.Reading training and test set
+# 02.Combining training, test set
+# 03.Merging activity_type with data from step 2. to include descriptive activity names 
+# 04.Labeling the variables with descriptive names 
+# 05.Creating 2nd Independent data set by aggegrating the table by activity & subject id per participant
+# 06.Exporting the tidyData set 
 
-# The function Inicio() indicates the local directory where the files are
+##########################################################################################################
 
-Inicio <-function () { WorkDirStr<-"D:/A Data Science/Getting and Cleaning Data/Peer Assessment"; WorkDirStr}
+# Reading training and test set
 
-# UCI dataset has X values, Y values and Subject IDs.
+#setting working directory
+setwd("~/Dropbox/edu/coursera/DATA_SPECIALIZATION/getdata/UCI HAR Dataset/");
 
-# Reading the x and y data
-# Funtion LeDados has two parameters: subdirectory name: where the files are; and name of file: "test" or "train" 
+# Reading Data
+features  = read.table('./features.txt',header=FALSE); #imports Features File
+activityType = read.table('./activity_labels.txt',header=FALSE); #imports activity labels file
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE); #imports subjectTrain file
+xTrain = read.table('./train/x_train.txt',header=FALSE); #imports xTrain file
+yTrain = read.table('./train/y_train.txt',header=FALSE); #imports yTrain file
 
-LeDados <- function (diretorio, opcao) {
-  
-  # mouting the subdirectory + the name of the Y file
-  # reading the Y file
-  montagem<-paste0(Inicio(), "/", diretorio, "/y_", opcao, ".txt") 
-  montagem<-file.path(montagem)
-  dados_y<-read.table(montagem, header=F, col.names=c("ActivityID"))
-  
-  # mounting the subdirectory + the name of subject ids file
-  # reading the subject ids file
+# Assigining column names to the imported Training data
+colnames(activityType)  = c('activityId','activityType');
+colnames(xTrain)        = features[,2]; 
+colnames(yTrain)        = "activityId";
+colnames(subjectTrain)  = "subjectId";
 
-  montagem<-paste0(Inicio(), "/", diretorio, "/subject_", opcao, ".txt")
-  montagem<-file.path(montagem)
-  dados_subject<- read.table(montagem, header=F, col.names=c("SubjectID"))
-  
-  # File "features.txt" has the 561 column names. Selecting columns MeasureID and MeasureName
-  montagem<-paste0(Inicio(), "/features.txt") 
-  montagem<-file.path(montagem)
-  dados_columns <- read.table(montagem, header=F, as.is=T, col.names=c("MeasureID","MeasureName"))
-  
-  # mouting the subdirectory + the name of the X file
-  # reading the X files
+# creating the final Training set by merging the xTrain, yTrain and subjectTrain sets
+trainingData = cbind(yTrain,subjectTrain,xTrain);
 
-  montagem<-paste0(Inicio(), "/", diretorio, "/X_", opcao, ".txt") 
-  montagem<-file.path(montagem)    
-  separados<- read.table(montagem, header=F, col.names=dados_columns$MeasureName)
-  
-  # getting the subset of requied columns names
-  subset_name_columns<- grep(".*mean\\(\\)|.*std\\(\\)", dados_columns$MeasureName)
-  
-  # subset the data (done early to save memory)
-  separados<- separados[,subset_name_columns]
-  
-  # append the activity id and subject id columns
-  separados$ActivityID <- dados_y$ActivityID
-  separados$SubjectID  <- dados_subject$SubjectID
-  
-  # return the data mounted in the data frame "separados"
-  separados  
-}
+# reading the test data ; since the data doesn't have column names, header=FALSE
+subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subjectTest file
+xTest       = read.table('./test/x_test.txt',header=FALSE); #imports xTest file
+yTest       = read.table('./test/y_test.txt',header=FALSE); #imports yTest file
 
-# ------------------------------------------------------------------------------------------------
+# assigining column names to Test data
+colnames(xTest)       = features[,2]; 
+colnames(yTest)       = "activityId";
+colnames(subjectTest) = "subjectId";
 
-# Begin of script
+# creating the final Test set by merging the xTest, yTest and subjectTest sets
+testData = cbind(yTest,subjectTest,xTest);
 
-# Read test data set, in a folder with name "/test", and all the files with names "test" on it
-dirop<-c("test", "test")
-LeDadosTeste <-LeDados(dirop[1], dirop[2])
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
+  # 02.Combining training, test set to create a final data set
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
 
-# Read training data sets, in a folder with name "/train", and all the files with names "train" on it
-dirop<-c("train", "train")
-LeDadosTreino <-LeDados(dirop[1], dirop[2])
+#Concatenating the training & test data sets
+finalData = rbind(trainingData,testData);
 
-# Resolving "1" Merging the training and test sets to create one data set.
-junta_dados <- rbind(LeDadosTeste, LeDadosTreino)
-cn <- colnames(junta_dados)
-cn <- gsub("\\.+mean\\.+", cn, replacement="Mean")
-cn <- gsub("\\.+std\\.+", cn, replacement="Std")
-colnames(junta_dados) <- cn
-                     
-# Resolving "3.": Using descriptive activity names to name the activities in the data set
-# Resolving "4": Appropriately labels the data set with descriptive activity names. 
-# Combine training+test data sets and add "activity" label as another column
-montagem<-paste0(Inicio(), "/activity_labels",".txt")
-montagem<-file.path(montagem)
-nome_atividades<- read.table(montagem, header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
-nome_atividades$ActivityName <- as.factor(nome_atividades$ActivityName)
-poem_nome<- merge(junta_dados, nome_atividades)
-               
-# Resolving "5.":Creates a second, independent tidy data set with the average of each variable 
-# for each activity and each subject.   
-# consolidating the dataset
-ides<- c("ActivityID", "ActivityName", "SubjectID")
-medidas<- setdiff(colnames(poem_nome), ides)
-consolidado<- melt(poem_nome, id=ides, measure.vars=medidas)                     
+# Storing the column names from the finalData into a character vector 'colNames',
+# this will be used to select the desired mean() & stddev() columns
+colNames  = colnames(finalData); 
 
-# Finaly, a tidy data format!
-consolidado<-dcast(consolidado, ActivityName + SubjectID ~ variable, mean)  
+# Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others
+logicalVector = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames));
 
-montagem<-paste0(Inicio(), "/jpa_tidy_data",".txt")
-write.table(consolidado, montagem)
+# subset finalData table based on the logicalVector to keep only desired columns
+finalData = finalData[logicalVector==TRUE];
+
+#+++++++++++++++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++++++++++++++#
+  # 03.Merging activity_type with data from step 2. to include descriptives activity names 
+#+++++++++++++++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++++++++++++++#
+
+# merge the finalData set with the acitivityType table to include descriptive activity names
+finalData = merge(finalData,activityType,by='activityId',all.x=TRUE);
+
+# Updating the colNames vector to include the new column names after merge
+colNames  = colnames(finalData); 
+
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
+  # 04. Labeling the variables with descriptive names
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
+
+# Cleaning up the variable names to make them more descriptive
+for (i in 1:length(colNames)) 
+{
+   colNames[i] = gsub("\\()","",colNames[i])
+   colNames[i] = gsub("-std$","StdDev",colNames[i])
+   colNames[i] = gsub("-mean","Mean",colNames[i])
+   colNames[i] = gsub("^(t)","time",colNames[i])
+   colNames[i] = gsub("^(f)","freq",colNames[i])
+   colNames[i] = gsub("([Gg]ravity)","Gravity",colNames[i])
+   colNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
+   colNames[i] = gsub("[Gg]yro","Gyro",colNames[i])
+   colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
+   colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
+   colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
+   colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
+};
+
+# reassigning the new descriptive column names to the finalData set
+colnames(finalData) = colNames;
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++++++++++++++++++++++#
+  # 05. Creating 2nd Independent data set by aggegrating the table by activity & subject id per participant
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# Creating a new table, finalData2 without the activityType column as a precursor to the next step for aggregating the finalData set
+finalData2  = finalData[,names(finalData) != 'activityType'];
+
+# Summarizing the finalData2 table to include only the average of each variable for each activity and each subject
+tidyData    = aggregate(finalData2[,names(finalData2) != c('activityId','subjectId')],by=list(activityId=finalData2$activityId,subjectId = finalData2$subjectId),mean);
+
+# merging the tidyData with activityType to include descriptive acitvity names
+tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE);
+
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
+# 06.Exporting the tidyData set 
+#++++++++++++++++++++++++++++++++++#++++++++++++++++++++++++++++++#
+write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t');
 
